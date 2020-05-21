d9951bff388cb6652fb27a94be8e0b6777b87c38
Juan Francisco Chango <jchango@udlanet.ec>
2020-05-21 01:25:50 -0400

    added cublas_vs_cufft.cpp

--- gpu_comparision/cublas_vs_cufft.cpp
+++ gpu_comparision/cublas_vs_cufft.cpp
@@ -142,8 +142,8 @@ void cufft_double_fft(int &dim_y, int &dim_x, double *x_n){
 int main(int argv, char** argc){
     printf("Comparing DCT cublas and cufft\n");
 
-    int size_m = 1024;
-    int size_n = 1024;
+    int size_m = 32;
+    int size_n = 64;
 
     int dim_y = size_m;
     int dim_x = size_n;
